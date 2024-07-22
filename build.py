bf581063ded78914467d23ae21d242aa5d9be492
nazennash <nazennash120@gmail.com>
2024-07-22 15:32:07 +0300

    build process

---
+++ build.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+
+#load data
+#split data
+#build model
+#compile model
+#train model
+#evaluate model
+#save model
